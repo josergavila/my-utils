69ee6f59be4fd9abcfd78be49e845402ec430662
josergavila <jose.avila@vorp.com>
2022-04-20 11:34:34 -0300

    renamed python folder; finished refactoring function to write df to excel

---
+++ my_python_utils/__init__.py
@@ -0,0 +1,5 @@
+from csv_utils import *
+from excel_utils import *
+from file_utils import *
+from pickle_utils import *
+from scrap_utils import *
--- my_python_utils/excel_utils.py
+++ my_python_utils/excel_utils.py
@@ -14,48 +14,6 @@ import numpy as np
 from file_utils import check_if_file_exists
 
 
-def _copy_excel_cell_range(
-    src_ws: openpyxl.worksheet.worksheet.Worksheet,
-    min_row: int = None,
-    max_row: int = None,
-    min_col: int = None,
-    max_col: int = None,
-    tgt_ws: openpyxl.worksheet.worksheet.Worksheet = None,
-    tgt_min_row: int = 1,
-    tgt_min_col: int = 1,
-    with_style: bool = True,
-) -> openpyxl.worksheet.worksheet.Worksheet:
-    """
-    copies all cells from the source worksheet [src_ws] starting from [min_row] row
-    and [min_col] column up to [max_row] row and [max_col] column
-    to target worksheet [tgt_ws] starting from [tgt_min_row] row
-    and [tgt_min_col] column.
-    """
-    if tgt_ws is None:
-        tgt_ws = src_ws
-
-    for row in src_ws.iter_rows(
-        min_row=min_row, max_row=max_row, min_col=min_col, max_col=max_col
-    ):
-        for cell in row:
-            if type(cell).__name__ == "MergedCell":
-                continue
-            tgt_cell = tgt_ws.cell(
-                row=cell.row + tgt_min_row - min_row - 1,
-                column=cell.col_idx + tgt_min_col - 1,
-                value=cell.value,
-            )
-            if with_style and cell.has_style:
-                # tgt_cell._style = copy(cell._style)
-                tgt_cell.font = copy(cell.font)
-                tgt_cell.border = copy(cell.border)
-                tgt_cell.fill = copy(cell.fill)
-                tgt_cell.number_format = copy(cell.number_format)
-                tgt_cell.protection = copy(cell.protection)
-                tgt_cell.alignment = copy(cell.alignment)
-    return tgt_ws
-
-
 def get_sheets_in_workbook(wb: openpyxl.workbook) -> dict:
     return {ws.title: ws for ws in wb.worksheets}
 
@@ -141,18 +99,14 @@ def write_df_to_excel(
     fmt_date: str = "dd-mm-yyyy",
     fmt_datetime: str = "dd-mm-yyyy hh:mm",
     clear_sheet_values: bool = False,
+    skip_df_columns: bool = True,
     **to_excel_kwargs,
 ) -> None:
     """
     Append a DataFrame [df] to existing Excel file [filename]
     into [sheet_name] Sheet. If [filename] doesn't exist, then this function will create it.
 
-    storage_options: dict, optional
-        Extra options that make sense for a particular storage connection, e.g. host, port,
-        username, password, etc., if using a URL that will be parsed by fsspec, e.g.,
-        starting “s3://”, “gcs://”.
     to_excel_kwargs: arguments which will be passed to `DataFrame.to_excel()`
-                            [can be a dictionary]
 
     Usage examples:
 
@@ -171,70 +125,113 @@ def write_df_to_excel(
                            fmt_datetime="dd.mm.yyyy hh:mm")
 
     """
+    # fetching setting and formatting info
     file_exists = check_if_file_exists(filename)
     if file_exists:
         wb = load_workbook(filename)
         sheet_names = wb.sheetnames
         sheet_exists = sheet_name in sheet_names
-        sheets = get_sheets_in_workbook(wb)
+        if sheet_exists:
+            if not start_row:
+                start_row = get_max_row(ws=wb[sheet_name]) + 1
 
+            if not start_col:
+                start_col = get_start_col(ws=wb[sheet_name])
+    else:
+        start_row, start_col = 1, 1
+        to_excel_kwargs["startrow"], to_excel_kwargs["startcol"] = start_row, start_col
+
+    # copy df into a new spreadsheet
     with pd.ExcelWriter(
         filename,
-        # filename.with_suffix(".xlsx"),
         mode="a" if file_exists else "w",
-        if_sheet_exists="new" if file_exists else None,
+        if_sheet_exists="new" if (file_exists and sheet_exists) else None,
         date_format=fmt_date,
         datetime_format=fmt_datetime,
     ) as writer:
         if file_exists:
             writer.book = wb
+            if sheet_exists and clear_sheet_values:
+                writer.book = clear_sheet(wb=writer.book, sheet_name=sheet_name)
+            writer.sheets = get_sheets_in_workbook(wb)
 
-            if sheet_exists:
-                if not start_row:
-                    start_row = get_max_row(ws=writer.book[sheet_name]) + 1
-
-                if not start_col:
-                    start_col = get_start_col(ws=writer.book[sheet_name])
-
-                if clear_sheet_values:
-                    writer.book = clear_sheet(wb=writer.book, sheet_name=sheet_name)
-            writer.sheets = sheets
-        else:
-            # file doesn't exist -> create new one
-            start_row, start_col = 1, 1
-
-        # write out the dataframe to excelwriter
         df.to_excel(writer, sheet_name=sheet_name, **to_excel_kwargs)
         worksheet = writer.sheets[sheet_name]
 
         if add_filter:
             worksheet.auto_filter.ref = worksheet.dimensions
 
-        first_col = int(to_excel_kwargs.get("index", True)) + 1
         worksheet = format_data_in_sheet(
             ws=worksheet, df=df, index=to_excel_kwargs.get("index", True)
         )
 
-    # TO DO: fix append part of the function
+    # move (append) rows from new worksheet to the `sheet_name` worksheet
     if file_exists and sheet_exists:
-        # move (append) rows from new worksheet to the `sheet_name` worksheet
-        wb = load_workbook(filename)
-        # retrieve generated worksheet name
-        new_sheet_name = set(wb.sheetnames) - set(sheet_names)
-        if new_sheet_name:
-            new_sheet_name = list(new_sheet_name)[0]
-        # copy rows written by `df.to_excel(...)` to
-        _copy_excel_cell_range(
+        try:
+            new_sheet_name = list(set(wb.sheetnames) - set(sheet_names))[0]
+        except IndexError:
+            new_sheet_name = None
+
+        copy_cell_range(
             src_ws=wb[new_sheet_name],
             tgt_ws=wb[sheet_name],
-            tgt_min_row=start_row,
+            min_row=min_row or int(skip_df_columns) + 1,
+            start_row=start_row - int(skip_df_columns),
+            start_col=start_col,
             with_style=True,
-            min_row=min_row,
         )
-        # remove new (generated by Pandas) worksheet
         del wb[new_sheet_name]
         wb.save(filename)
         wb.close()
+
+
+def copy_cell_range(
+    src_ws: openpyxl.worksheet.worksheet.Worksheet,
+    tgt_ws: openpyxl.worksheet.worksheet.Worksheet = None,
+    min_row: int = None,
+    max_row: int = None,
+    min_col: int = None,
+    max_col: int = None,
+    start_row: int = 1,
+    start_col: int = 1,
+    with_style: bool = True,
+) -> openpyxl.worksheet.worksheet.Worksheet:
+    """copies cells from the source worksheet [src_ws] starting from [min_row] row
+    and [min_col] column up to [max_row] row and [max_col] column
+    to target worksheet [tgt_ws] starting from [start_row] row
+    and [start_col] column.
+    """
+    if tgt_ws is None:
+        tgt_ws = src_ws
+
+    for row in src_ws.iter_rows(
+        min_row=min_row, max_row=max_row, min_col=min_col, max_col=max_col
+    ):
+        for src_cell in row:
+            if type(src_cell).__name__ == "MergedCell":
+                raise ValueError("Source worksheet should not have Merged Cells")
+
+            tgt_cell = tgt_ws.cell(
+                row=src_cell.row + start_row - 1,
+                column=src_cell.col_idx + start_col - 1,
+                value=src_cell.value,
+            )
+            if with_style and src_cell.has_style:
+                replicate_cell_style(tgt_cell, src_cell)
+
+    return tgt_ws
+
+
+def replicate_cell_style(
+    tgt_cell: openpyxl.cell.cell, src_cell: openpyxl.cell.cell
+) -> None:
+    tgt_cell.font = copy(src_cell.font)
+    tgt_cell.border = copy(src_cell.border)
+    tgt_cell.fill = copy(src_cell.fill)
+    tgt_cell.number_format = copy(src_cell.number_format)
+    tgt_cell.protection = copy(src_cell.protection)
+    tgt_cell.alignment = copy(src_cell.alignment)
+    return
 
 
 if __name__ == "__main__":
@@ -242,6 +239,6 @@ if __name__ == "__main__":
     df = pd.DataFrame([[1000, 20], [30, 40]])
     filename = "tim.xlsx"
     sheet_name = "Grades"
-    to_excel_kwargs = {"index": False}
-    # to_excel_kwargs = {}
-    write_df_to_excel(filename, df, sheet_name, **to_excel_kwargs)
+    # to_excel_kwargs = {"index": False}
+    to_excel_kwargs = {}
+    write_df_to_excel(filename, df, sheet_name, skip_df_columns=True, **to_excel_kwargs)
